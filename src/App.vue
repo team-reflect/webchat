13debe27f9bb34b75137b536b3cc36490c931d34
Alex MacCaw <alex@alexmaccaw.com>
2024-10-25 10:07:05 -0400

    Fix dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="flex flex-col h-screen overflow-hidden user-select-none bg-background">
-    <!-- Nav bar -->
-    <div class="flex justify-end p-1 px-2 border-b border-system-border gap-2 flex-none">
-      <Button @click="toggleConfig" variant="ghost" size="icon" class="w-8 h-8">
-        <CircleX v-if="showConfig" class="w-4 h-4" />
-        <Settings v-else class="w-4 h-4" />
-      </Button>
-    </div>
+  <div :class="darkMode ? 'dark' : ''" class="h-screen overflow-hidden">
+    <div class="flex flex-col h-screen overflow-hidden user-select-none bg-background">
+      <!-- Nav bar -->
+      <div
+        class="flex justify-end p-1 px-2 border-b border-system-border gap-2 flex-none"
+      >
+        <Button @click="toggleConfig" variant="ghost" size="icon" class="w-8 h-8">
+          <CircleX v-if="showConfig" class="w-4 h-4" />
+          <Settings v-else class="w-4 h-4" />
+        </Button>
+      </div>
 
-    <div class="flex-1 flex flex-col overflow-hidden">
-      <ConfigScreen v-if="showConfig" @config-saved="onConfigSaved" />
-      <ChatScreen v-else />
+      <div class="flex-1 flex flex-col overflow-hidden">
+        <ConfigScreen v-if="showConfig" @config-saved="onConfigSaved" />
+        <ChatScreen v-else />
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +26,9 @@ import Button from '@/components/ui/button/Button.vue'
 import { Settings, CircleX } from 'lucide-vue-next'
 import ChatScreen from '@/screens/ChatScreen.vue'
 import ConfigScreen from '@/screens/ConfigScreen.vue'
+import { usePreferredDark } from '@vueuse/core'
+
+const darkMode = usePreferredDark()
 
 const config = useConfig()
 
